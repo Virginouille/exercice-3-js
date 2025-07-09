0a63d4b6dd8cfad85378212dfe78e8e166695348
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-07-09 11:11:08 +0200

    structure classe tache

---
+++ main.js
@@ -0,0 +1,6 @@
+import { afficher } from './modules/tache.js';
+
+//Ajout tache dans main pour tester
+const tache = new Tache("Vacances", "Partir en vacances", new Date(2025, 7, 15));
+
+tache.afficher();
---
+++ modules/tache.js
@@ -0,0 +1,64 @@
+class Tache {
+
+    //Attributs
+    #titre = "";
+    #texte = "";
+    #date = null;
+
+
+    //Constructeur
+    constructor(titre, texte, date) {
+        this.#titre = titre;
+        this.#texte = texte;
+        this.#date = date;
+    }
+
+    /**Getters et setters */
+    get titre() {
+        return this.#titre;
+    }
+
+    set titre(nouveautTitre) {
+        this.#titre = nouveautTitre;
+    }
+
+    get texte() {
+        return this.#texte;
+    }
+
+    set texte(nouveauTexte) {
+        this.#texte = nouveauTexte;
+    }
+
+    get date() {
+        return this.#date;
+    }
+
+    set date(nouvelleDate) {
+        this.#date = nouvelleDate;
+    }
+}
+
+
+/**Méthode afficher() 
+  * @return une chaine descriptive
+ */
+
+function afficher() {
+    return {
+        "Titre": tache.titre,
+        "Texte": tache.texte,
+        "Date": tache.date
+    };
+}
+
+/**Méthode contientMot(mot) 
+ * @return retourne true si le mot est dans le titre ou le texte insensible à la casse/accents
+*/
+
+/**Méthode estEntreDates(dateDebut, dateFin) 
+ * @return true si la tâche est dans l'intervalle
+*/
+
+//Exports module
+export { afficher };
